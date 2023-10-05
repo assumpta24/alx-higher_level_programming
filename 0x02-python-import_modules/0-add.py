1d6eadc4db9b848da1185ec7c90a6e9bc24b2dbf
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-10-05 13:22:41 -0700

    a program that imports the function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -6,6 +6,6 @@ b = 2
 if __name__ == "__main__":
     from add_0 import add
 
-    #prints the result of the addition 1 + 2 = 3
+    # prints the result of the addition 1 + 2 = 3
 
     print("{} + {} = {}".format(a, b, add(a, b)))
