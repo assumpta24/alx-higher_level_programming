9ae79175e7a1c9d0d9d828382b41f461864e4240
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-10-05 13:09:05 -0700

    a program that imports functions from the file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+a = 10
+b = 5
+if __name__ =="__main__":
+    from calculator_1 import addition, subtraction, multiplication, division
+    print("{} + {} = {}".format(a, b, addition(a, b)))
+    print("{} - {} = {}".format(a, b, subtraction(a, b)))
+    print("{} * {} = {}".format(a, b, multiplication(a, b))
+            print("{} / {} = {}".format(a, b, division(a, b)))
